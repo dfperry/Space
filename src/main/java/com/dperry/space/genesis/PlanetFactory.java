43a000d7b6b0dd60cd8d268a86de13ddfe49c7da
Dan Perry <lobotomize@gmail.com>
2012-02-03 13:30:16 -0500

    factories

---
+++ src/main/java/com/dperry/space/genesis/PlanetFactory.java
@@ -0,0 +1,11 @@
+package com.dperry.space.genesis;
+
+import com.dperry.space.model.space.Planet;
+
+public class PlanetFactory {
+
+	public Planet createPlanet() {
+		
+		return new Planet();
+	}
+}
--- src/main/java/com/dperry/space/model/space/Planet.java
+++ src/main/java/com/dperry/space/model/space/Planet.java
@@ -2,4 +2,5 @@ package com.dperry.space.model.space;
 
 public class Planet {
 
+	int size;
 }
